05241f050f4f9be0e294f78702dda0872933150c
igor2890 <igor2890@mail.ru>
2022-04-17 09:38:53 +0300

    moved files

---
+++ GBShop/Core/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  GBShop
+//
+//  Created by Игорь Андрианов on 17.04.2022.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
